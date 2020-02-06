70edf75f0b79606a3e3b607707c87941b6989d29
jdluong <luongjohnd@gmail.com>
2020-02-05 22:58:33 -0800

    added argument for lambda

--- api_url.py
+++ api_url.py
@@ -18,7 +18,7 @@ class API:
 
     BLOG_URL = ADMIN_URL+"/blogs.json"
 
-    ARTICLE_URL = lambda blog_id: f"{ADMIN_URL}/blogs/#{blog_id}/articles.json"
+    ARTICLE_URL = lambda ADMIN_URL, blog_id: f"{ADMIN_URL}/blogs/#{blog_id}/articles.json"
     ARTICLE_URL_count = lambda blog_id: f"{ADMIN_URL}/blogs/#{blog_id}/articles/count.json"
     ARTICLE_URL_get_single = lambda blog_id, article_id: f"{ADMIN_URL}/blogs/#{blog_id}/articles/#{article_id}.json"
 
