ccbd5aae7b916cc45579ad5f1a789b519e2e67a6
Agnes Jang <agnesj@uci.edu>
2020-02-05 21:02:20 -0800

    add cors

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 import psycopg2
 app = Flask(__name__)
+CORS(app)
 DATABASE_URL = "postgres://rdrhinxhcqfrkm:989d6c91e8eb163284de44343083d0c4928b4d539e493bb8dffbe16ce29e994d@ec2-52-203-98-126.compute-1.amazonaws.com:5432/d8u52i7luh8cuv?sslmode=require"
 
 @app.route('/')
@@ -9,6 +11,14 @@ def hello():
 
 @app.route('/blog_posts')
 def fetch_blog_posts():
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
+    cur.execute("select * from blog_post;")
+    posts = cur.fetchall()
+    return str(posts)
+
+@app.route('/add_post')
+def add_blog_post():
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
     cur.execute("select * from blog_post;")
