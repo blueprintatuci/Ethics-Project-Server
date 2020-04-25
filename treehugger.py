36902906ccf2378a0447984ef7d5690ed98a5b78
judithdrup <judithdrup@gmail.com>
2020-04-24 23:56:52 -0700

    fix treehugger publish_date, add zerowastehome

--- treehugger.py
+++ treehugger.py
@@ -2,12 +2,34 @@ from bs4 import BeautifulSoup
 import requests
 
 ## for treehugger.com
-## does not include featured article
+## does not include featured article, can include all articles once duplicate catcher is in place?
+ 
+months = {
+    'January': 1,
+    'February': 2,
+    'March': 3,
+    'April': 4,
+    'May': 5,
+    'June': 6,
+    'July': 7,
+    'August': 8,
+    'September': 9,
+    'October': 10,
+    'November': 11,
+    'December' :12
+}
 
-def treehugger():
+def date_convert(string):
+    date = string.split()
+    mon = months[date[0]]
+    day = date[1].strip(',')
+    year = date[2]
+    return "{}/{}/{}".format(mon, day, year)
+
+
+def scrape_treehugger():
     l = []
     
-       
     base_url = 'https://www.treehugger.com/' 
     print(base_url)
 
@@ -15,34 +37,30 @@ def treehugger():
     r = requests.get(base_url)
     soup = BeautifulSoup(r.text, "html.parser")
 
-    all_product = soup.find_all('article', class_="c-article c-article--card")
-    print(len(all_product))
+    latest_stories = soup.find_all('article', class_="c-article c-article--card")
 
-    for item in all_product:
+    for item in latest_stories:
         d = { }
-        # item = item.find("div", class_="c-article__container")
-        # 'url' : 'https://www.treehugger.com' + art.css('div.c-article__image a::attr(href)').get(),
+
+        ## url
         url = item.find("div", class_ = "c-article__image").find("a")
         d['url'] = 'https://www.treehugger.com' + url['href']
 
-        # 'title' : art.css('div.c-article__summary a::text').get().strip(),
+        ## title
         title = item.find("div", class_="c-article__summary").find("a").get_text().strip('\n')
         d['title'] = title
 
-        # 'author' : art.css('div.c-article__summary div.c-article__byline a::text').get(),
+        ## author
         author = item.find("div", class_='c-article__summary').find("div", class_='c-article__byline').find('a').get_text()
         d['author'] = author
 
-        # 'image_url' : art.css('div.c-article__image img::attr(src)').get(),
+        ## image_url
         image_url = item.find("div", class_ = "c-article__image").find("img")
         d['image_url'] = image_url['src']
 
-        # 'publish_date' : date_convert(art.css('div.c-article__byline span a::text')[-1].get()),
+        ## publish_date
         publish_date = item.find("div", class_="c-article__byline").find('a', class_="c-article__published").get_text()
-        d['publish_date'] = publish_date
-
-        # 'source': self.name,
-        d['source'] = 'treehugger'
+        d['publish_date'] = date_convert(publish_date)
 
         l.append(d)
 
@@ -51,3 +69,4 @@ def treehugger():
 
 if __name__ == "__main__":
     print(treehugger())
+    
---
+++ zerowastehome.py
@@ -0,0 +1,72 @@
+from bs4 import BeautifulSoup
+import requests
+
+## for zerowastehome.com
+ 
+months = {
+    'January': 1,
+    'February': 2,
+    'March': 3,
+    'April': 4,
+    'May': 5,
+    'June': 6,
+    'July': 7,
+    'August': 8,
+    'September': 9,
+    'October': 10,
+    'November': 11,
+    'December' :12
+}
+
+def date_convert(string):
+    date = string.split()
+    mon = months[date[0]]
+    day = date[1].strip(',')
+    year = date[2]
+    return "{}/{}/{}".format(mon, day, year)
+
+
+def scrape_zerowastehome():
+    l = []
+    
+    base_url = 'https://zerowastehome.com/blog/' 
+    print(base_url)
+
+    # Request URL and Beautiful Parser
+    r = requests.get(base_url)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    blogposts = soup.find_all('div', class_="post-content")
+
+    for item in blogposts:
+        d = { }
+
+        ## url
+        url = item.find("a", class_="entire-meta-link")
+        d['url'] = url['href']
+
+        ## title
+        title = item.find("h3", class_="title").get_text()
+        d['title'] = title.strip()
+
+        ## author
+        author = item.find("div", class_="text").find("a").get_text()
+        d['author'] = author
+
+        ## image_url
+        image_url = item.find("span", class_='post-featured-img') 
+        #image_url['style'] = ''background-image: url(https://s3-us-east-2.amazonaws.com/zerowaste-media/wp-content/uploads/20180803124650/2018-07-01-06.32.13-900x600.jpg);'
+        d['image_url'] = image_url['style'].strip("background-image: url(").strip(");")
+
+        ## publish_date
+        publish_date = item.find("div", class_="text").find("span").get_text()
+        d['publish_date'] = date_convert(publish_date)
+
+        l.append(d)
+
+    return l
+
+
+if __name__ == "__main__":
+    print(scrape_zerowastehome())
+    
